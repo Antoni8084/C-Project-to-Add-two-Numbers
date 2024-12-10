4990e1d72ce55c628666947da9b4fb400e9b1fbf
Antoni <tanganyikaantoni1@gmail.com>
2024-12-10 14:12:04 +0300

    started the Project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a=0;
+    int b=0;
+    int c=0;
+    cout<<"Enter the First Number:"<<endl;
+    cin>>a;
+    cout<<"Enter the Second Number"<<endl;
+    cin>>b;
+    c = a+b;
+    cout<<"The Sum is "<<c<<endl;
+}
